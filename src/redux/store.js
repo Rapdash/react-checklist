c4170efd9d592e357b98052fe98065338f96f240
Frederick Johnson <fritzj96@gmail.com>
2019-12-13 12:58:26 -0800

    added configureStore()

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,11 @@ import thunk from "redux-thunk";
 
 import { applyMiddleware, createStore } from "redux";
 
+import { verifyAuth } from "./actions";
 import { rootReducer } from "./reducers";
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const configureStore = () => {
+  const store = createStore(rootReducer, applyMiddleware(thunk));
+  store.dispatch(verifyAuth()); // Checks for auth state
+  return store;
+};
